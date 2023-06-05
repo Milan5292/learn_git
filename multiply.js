70e5cc484d77cce93fe2134495db7cc963110232
Milan Chaudhary <milanchaudhary968@gmail.com>
2023-06-05 16:47:09 +0530

    multiply commited

---
+++ multiply.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+        <script>
+        var d =10;
+        var j=20;
+
+        var z = d * j;
+
+    document.write("sum of two number is :" + z);
+        </script>
+    </head>
+</html>
